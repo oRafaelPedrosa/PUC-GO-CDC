65100da9ccfe61aebe409f4d3a1aa80ae2d9a2d9
Victor P. Araújo <37886774+VictorArajooj@users.noreply.github.com>
2019-08-16 21:41:02 -0300

    Update base.cpp

--- 4° Semestre/Fundamentos 4/Trabalhos/1 Semana/codigos/base.cpp
+++ 4° Semestre/Fundamentos 4/Trabalhos/1 Semana/codigos/base.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stack>
 #include <math.h>
+#include <string.h>
+#include <string>
 using namespace std;
 
 Bin(int N)
@@ -56,11 +58,49 @@ QualquerBase(int N, int base1,int base2)
 	
 	cout << Soma << endl;
 }
+#include <sstream>
+#include <algorithm>
+
+
+QualquerBase_string(double N, int base1, int base2)
+{
+	
+  	//string resultado =  to_string(N);
+  	//cout << resultado;
+  	
+  	ostringstream strs;
+	double dbl = N;
+	strs << dbl;
+	string str = strs.str();
+	//cout << str;
+	
+	str.erase(std::remove(str.begin(), str.end(), '.'), str.end());
+	//cout << str;
+	
+	int cont = 0;
+	int m=0;
+	m = atoi(str.c_str());
+	while(N)
+	{
+		N = N/base2;
+		cont++;
+	}
+	int Aux, Soma = 0;
+	for(int i=0; i<cont; i++)
+	{
+		Aux = m%base2;
+		Soma += Aux* (pow(base1,i));
+		m = m/base2;
+	}
+	
+	cout << Soma << endl;
+}
 
 
 main ()
 {
-	Decimal(1010);
-	Bin(10);
-	QualquerBase(0010,2,8);	
+	//Decimal(1010);
+	//Bin(10);
+	//QualquerBase(0010,2,8);
+	QualquerBase_string(13.5,10,8);	
 }
