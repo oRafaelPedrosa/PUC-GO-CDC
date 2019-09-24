7c1523cbc0be7e282d85a8592884466819d6838f
Victor P. Araújo <37886774+VictorArajooj@users.noreply.github.com>
2019-09-24 20:33:05 -0300

    Create mantissa.cpp

---
+++ 4° Semestre/Fundamentos 4/Trabalhos/mantissa/mantissa.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    double pontoFlutuante;
+    cout << "insira um numero: ";
+    cin >> pontoFlutuante;
+    cout << endl << endl;
+    cout << "Numero com ponto Flutuante: " << pontoFlutuante << endl;
+
+
+    int parteInteira=pontoFlutuante;
+
+    string parteInteiraToBin = bitset<8>(parteInteira).to_string();
+    //cout << parteInteiraToBin << endl;
+
+    int parteInteiraToBinInt=stoi(parteInteiraToBin);
+
+    cout << "Parte inteira para binario: " << parteInteiraToBinInt << endl;
+
+    float manticaPt1=pontoFlutuante-parteInteira;
+    cout << "Parte flutuante: " << manticaPt1 << endl;
+
+    // tabela
+
+    cout << endl << endl;
+    cout << " == TABELA ==" << endl;
+    bool parar=false;
+    vector<int> jafoi;
+    vector<int> INT;
+
+    float manticaPt2=manticaPt1;
+
+
+    int cont=6;
+
+    int primeiro;
+    int cont2=1;
+    while(parar!=true)
+    {
+        double x2;
+
+        // x2
+
+        x2=manticaPt2*2;
+        //int jafoi2=x2*10;
+
+
+
+
+
+
+
+        cout << "x2: " << x2 << endl;
+
+        // INT
+
+        int parteInteira=x2;
+        cout << "Parte Inteira: " << parteInteira << endl;
+
+
+        int jafoi2=(x2-parteInteira)*10;
+        jafoi.push_back(jafoi2);
+        INT.push_back(parteInteira);
+
+        if(cont2==1)
+        {
+            primeiro=jafoi2;
+            //jafoi.pop_back();
+        }
+        cont2--;
+        //cout << primeiro << endl;
+
+
+        manticaPt2=x2-parteInteira;
+
+        //int achar=manticaPt2*10;
+
+
+
+        if(jafoi.back()!=primeiro)
+        {
+            //cout << "entrou?" << endl;
+            for(int i=1; i<jafoi.size();i++)
+            {
+                //cout << "jafoi: " << jafoi[i] << endl;
+                //cout << "primeiro: " << primeiro << endl;
+                if(primeiro==jafoi[i])
+                {
+                    parar=true;
+                }
+            }
+        }
+
+
+
+
+       //cont--;
+       //if(cont==0)parar=true;
+
+    }
+    cout << " == TABELA == " << endl;
+    cout << endl;
+
+    string final0;
+    cout << "Ponto Flutuante: ";
+    INT.pop_back();
+    for(int i=0; i<INT.size(); i++)
+    {
+        cout << INT[i];
+        final0+=to_string(INT[i]);
+    }
+    cout << endl;
+
+    string final1;
+    final1+=to_string(parteInteiraToBinInt);
+    final1+=",";
+    final1+=final0;
+
+    cout << endl;
+    cout << "Final: ";
+    cout << final1 << endl;
+
+}
+
